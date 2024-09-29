09cc1042cfbde6c7014bd1391b7a4c9947f43e3c
Daniel <avilam.daniel@gmail.com>
2024-09-29 16:25:34 -0500

    Added dashboard page and header component with their calls

--- src/po/components/index.js
+++ src/po/components/index.js
@@ -1,8 +1,11 @@
+const Header = require('../components/common/header.component')
+
 const LoginBox = require('../components/login/login-box.component')
 const ErrorMessageBox = require('../components/login/error-message.component')
 const CredentialsInfo = require('../components/login/credentials-info.component')
 
 module.exports = {
+    Header,
     LoginBox,
     ErrorMessageBox,
     CredentialsInfo,
